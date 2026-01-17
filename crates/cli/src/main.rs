b848ee954a89981e40a0c4bfcc6f48c5680a6090
Scotte Zinn <scotte@zinn.ca>
2026-01-17 14:27:20 -0500

    feat(cli): Command arguments framework

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,10 +1,14 @@
-use anyhow::{Context, Result};
+use anyhow::Context;
+use anyhow::Result;
+use hex_play::args::Args;
 use hex_play::config::Config;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args: Args = clap::Parser::parse();
     let config = Config::load().context("Cannot load configuration")?;
     println!("{:?}", config);
+    println!("{:?}", args);
 
     Ok(())
 }
